a1a17413728ae06b28bfedd628b06c409224a804
Fernanda Pereira Pedro <fernandappedro@gmail.com>
2024-07-22 16:31:15 -0300

    Componente: Post

---
+++ src/components/Post.tsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.outhor}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/109635410?v=4" />
+          <div className={styles.outhorInfo}>
+            <strong>Fernanda Pereira Pedro</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="22 de Julho as 16:16" dateTime="2024-07-22 15:59" >Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
